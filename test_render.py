28bfe7be9a5ba728ddf9253e6fc8adc671e5bc7d
Marty0427 <93676663+Marty0427@users.noreply.github.com>
2023-02-09 14:36:42 +0100

    Update test_render.py

--- test_render.py
+++ test_render.py
@@ -1,8 +1,11 @@
 from dash import Dash, html, dcc, Input, Output
-
+import flask
 import pandas as pd
 
-app = Dash(__name__)
+server = flask.Flask(__name__) # define flask app.server
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=server) # call flask server
+
 
 #df = pd.read_csv('../data/wheels.csv')
 
@@ -36,5 +39,4 @@ def callback_b(colors_value):
     return 'You\'ve selected "{}"'.format(colors_value)
 
 if __name__ == '__main__':
-    server = app.server
     app.run_server()
