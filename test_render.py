b3820e2832ab746e8e81bdb13763cb032e754689
Marty0427 <93676663+Marty0427@users.noreply.github.com>
2023-02-09 15:01:55 +0100

    Update test_render.py

--- test_render.py
+++ test_render.py
@@ -1,55 +1,23 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import pandas as pd
-import plotly.graph_objs as go
-import flask
+import dash_auth
+import dash_bootstrap_components as dbc
+from dash import dcc, html, Output, Input, State, dash_table
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__,
+                meta_tags=[
+                    {'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}],
+                external_stylesheets=[dbc.themes.QUARTZ])
 
-server = flask.Flask(__name__) # define flask app.server
+server = app.server
+app.title = 'Title'
+app.layout = dbc.Container(fluid=True, children=[])
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=server) # call flask server
+@app.callback(
+    [Output('some_stuff', 'children'),],
+    [Input('stuff', 'value')])
 
-# run following in command
-# gunicorn graph:app.server -b :8000
-
-
-df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/' +
-    '5d1ea79569ed194d432e56108a04d188/raw/' +
-    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
-    'gdp-life-exp-2007.csv')
-
-
-app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure={
-            'data': [
-                go.Scatter(
-                    x=df[df['continent'] == i]['gdp per capita'],
-                    y=df[df['continent'] == i]['life expectancy'],
-                    text=df[df['continent'] == i]['country'],
-                    mode='markers',
-                    opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) for i in df.continent.unique()
-            ],
-            'layout': go.Layout(
-                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-                yaxis={'title': 'Life Expectancy'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest'
-            )
-        }
-    )
-])
+def display_something(value):
+    return value
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
