fbd871dd3dc4439614700d9ae51bf199d2b0cc7b
Marty0427 <93676663+Marty0427@users.noreply.github.com>
2023-02-09 19:31:29 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -35,7 +35,6 @@ def get_data(trades_len = 10, period = 'mo2', ratio = 80, summary = pd.read_csv(
 
 def make_app():
     app = dash.Dash(__name__) # call flask server
-    server = app.server
     df = get_data()
 
     table = dash_table.DataTable(
@@ -85,4 +84,6 @@ def make_app():
 
 if __name__ == '__main__':
     app = make_app()
+    server = app.server
+
     app.run_server(debug=False)
