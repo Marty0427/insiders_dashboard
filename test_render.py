4de5efa109210e0f54f5dd1fe28186ec11759cab
Marty0427 <93676663+Marty0427@users.noreply.github.com>
2023-02-09 14:41:47 +0100

    Update test_render.py

--- test_render.py
+++ test_render.py
@@ -1,42 +1,55 @@
-from dash import Dash, html, dcc, Input, Output
-import flask
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 import pandas as pd
+import plotly.graph_objs as go
+import flask
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 server = flask.Flask(__name__) # define flask app.server
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=server) # call flask server
 
+# run following in command
+# gunicorn graph:app.server -b :8000
 
-#df = pd.read_csv('../data/wheels.csv')
+
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
+
 
 app.layout = html.Div([
-    dcc.RadioItems(
-        id='wheels',
-        options=[1, 2, 3],
-        value=1
-    ),
-    html.Div(id='wheels-output'),
-
-    html.Hr(),  # add a horizontal rule
-    dcc.RadioItems(
-        id='colors',
-        options=['red', 'green', 'blue'],
-        value='blue'
-    ),
-    html.Div(id='colors-output')
-], style={'fontFamily':'helvetica', 'fontSize':18})
-
-@app.callback(
-    Output('wheels-output', 'children'),
-    [Input('wheels', 'value')])
-def callback_a(wheels_value):
-    return 'You\'ve selected "{}"'.format(wheels_value)
-
-@app.callback(
-    Output('colors-output', 'children'),
-    [Input('colors', 'value')])
-def callback_b(colors_value):
-    return 'You\'ve selected "{}"'.format(colors_value)
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
